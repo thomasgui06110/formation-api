21aea702979dd6a7858cca57047b478e5903535c
Thomasgui <guilhem.thomas@gmail.com>
2019-09-23 16:51:20 +0200

    Ajout de l'authentification via JWT

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,9 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ApiResource
+ * @ApiResource(
+ *  normalizationContext={"groups"={"users_read"}}
+ * )
  * @UniqueEntity("email", message="Un utilisateur ayant ce mail existe déjà !")
  */
 class User implements UserInterface
@@ -22,13 +24,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"customers_read"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresource", "users_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresource"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresource", "users_read"})
      * @Assert\NotBlank(message="L'email doit $etre renseigné")
      * @Assert\Email(message="Mail non valide")
      */
@@ -49,7 +51,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresource"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresource", "users_read"})
      * @Assert\NotBlank(message="Le prénom doit $etre renseigné")
      * @Assert\Length(min=3, minMessage="Le prénom doit faire entre 3 et 255 caractères !", max=255, maxMessage="Le prénom doit faire au maximum 255 caractères" )
      */
@@ -57,7 +59,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read", "invoices_subresource"})
+     * @Groups({"customers_read", "invoices_read", "invoices_subresource", "users_read"})
      * @Assert\NotBlank(message="Le nom doit etre renseigné")
      *  @Assert\NotBlank(message="Le nom doit $etre renseigné")
      * @Assert\Length(min=3, minMessage="Le prnom doit faire entre 3 et 255 caractères !", max=255, maxMessage="Le nom doit faire au maximum 255 caractères" )
